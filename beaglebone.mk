e9d7f3515d3fcf5da18f3c2ed53c532e7c36dd58
Pankaj <pankaj.bharadiya@ti.com>
2011-11-24 21:56:13 +0530

    Revert "BEAGLEBONE: Add TRAGET_PRODUCT check in beaglebone.mk"
    This reverts commit 77e7e463d7e1caa36e35e427cd5a40038ba03035.

--- beaglebone.mk
+++ beaglebone.mk
@@ -16,7 +16,6 @@ PRODUCT_BRAND := TI
 PRODUCT_NAME := beaglebone
 PRODUCT_DEVICE := beaglebone
 PRODUCT_PACKAGE_OVERLAYS := device/rowboat/generic
-ifeq ($(TARGET_PRODUCT), beaglebone)
 # Lowering lcd density from 160 to 80 to support lower resolution for vnc screen
 ADDITIONAL_DEFAULT_PROPERTIES += ro.sf.lcd_density=80
-endif
+
